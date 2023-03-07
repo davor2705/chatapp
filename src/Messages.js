2be6ec19f86f367c2faab0b93fecdb19f22a9b6a
davor2705 <bacalja.davor@gmail.com>
2023-03-07 23:04:56 +0100

    second commit

--- src/Messages.js
+++ src/Messages.js
@@ -1,19 +1,6 @@
 import {Component} from "react";
 import React from "react";
 
-function randomName() {
-    const adjectives = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
-    const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
-    const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-    const noun = nouns[Math.floor(Math.random() * nouns.length)];
-    return adjective + noun;
-  }
-  
-  function randomColor() {
-    return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
-  }
-  
-
 class Messages extends Component {
   render() {
     const {messages} = this.props;
@@ -23,11 +10,8 @@ class Messages extends Component {
       </ul>
     );
   }
-}
 
-export default Messages;
-
-renderMessage(message) ;{
+  renderMessage(message) {
     const {member, text} = message;
     const {currentMember} = this.props;
     const messageFromMe = member.id === currentMember.id;
@@ -35,10 +19,10 @@ renderMessage(message) ;{
       "Messages-message currentMember" : "Messages-message";
     return (
       <li className={className}>
-        <span
-          className="avatar"
-          style={{backgroundColor: member.clientData.color}}
-        />
+      <span
+        className="avatar"
+        style={{backgroundColor: member.clientData.color}}
+      />
         <div className="Message-content">
           <div className="username">
             {member.clientData.username}
@@ -48,3 +32,6 @@ renderMessage(message) ;{
       </li>
     );
   }
+}
+
+export default Messages;
